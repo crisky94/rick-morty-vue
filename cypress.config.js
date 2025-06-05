e08cfca5eee8229aec7a5945005eee298fef4970
Cris <criistiiniilla.1994@gmail.com>
2025-06-05 17:01:07 +0200

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'mvcaqg'
+  projectId: 'mvcaqg',
+  e2e: {
+    setupNodeEvents(on, config) {
+    },
+    baseUrl: 'https://rick-morty-39iyxpr96-crisky94s-projects.vercel.app/', // <-- ¡PON AQUÍ LA URL DE TU APP!
+  },
 });
